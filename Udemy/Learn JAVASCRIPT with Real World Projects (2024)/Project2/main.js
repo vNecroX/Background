86b7f3a2eef05184ffad8ba887fca79e6c7469f7
darkঐু <a16100266@live.ceti.mx>
2024-02-16 20:34:18 -0600

    LJS2024-2

---
+++ Udemy/Learn JAVASCRIPT with Real World Projects (2024)/Project2/main.js
@@ -0,0 +1,8 @@
+let firstName = prompt('Please enter your first name: ')
+let lastName = prompt('Please enter your last name: ')
+let age = parseInt(prompt('Please enter your age: '))
+age += 2
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
