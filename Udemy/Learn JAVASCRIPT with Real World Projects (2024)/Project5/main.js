1655fecd1cdb670a8171ee14df616c257c999fc6
darkঐু <a16100266@live.ceti.mx>
2024-02-16 21:38:31 -0600

    LJS2024-5

---
+++ Udemy/Learn JAVASCRIPT with Real World Projects (2024)/Project5/main.js
@@ -0,0 +1,9 @@
+let friends = ['Alan', 'Mark', 'Julia', 'Bella']
+console.log(friends)
+
+friends[1] = 'Anna'
+friends[4] = 'Maverick'
+friends[5] = 1
+
+console.log(friends)
+console.log(friends[1])
