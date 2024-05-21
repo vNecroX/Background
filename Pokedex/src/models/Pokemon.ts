85fd87c265f33b4ca03773e4b9b860d401401008
darkঐু <estebanqq63@gmail.com>
2024-05-20 22:24:50 -0600

    First contact with Pokemon API

---
+++ Pokedex/src/models/Pokemon.ts
@@ -0,0 +1,55 @@
+export default class Pokemon {
+    id: number;
+    name: string;
+    image?: string;
+    height?: number;
+    weight?: number;
+    type?: string;
+    move?: string;
+    stats?: Stats;
+
+    constructor (
+        id: number,
+        name: string,
+        image?: string,
+        height?: number,
+        weight?: number,
+        type?: string,
+        move?: string,
+        stats?: Stats
+    ) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.height = height;
+        this.weight = weight;
+        this.type = type;
+        this.move = move;
+        this.stats = stats;
+    }
+}
+
+export class Stats {
+    hp: number;
+    attack: number;
+    defense: number;
+    specialAttack: number;
+    specialDefense: number;
+    speed: number;
+
+    constructor (
+        hp: number,
+        attack: number,
+        defense: number,
+        specialAttack: number,
+        specialDefense: number,
+        speed: number
+    ) {
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+        this.specialAttack = specialAttack;
+        this.specialDefense = specialDefense;
+        this.speed = speed
+    }
+}
